a7292c76cc76ab039fd5bdff527736ad43e88ab3
Chris Arsenault <chrisstanarsenault@gmail.com>
2025-04-15 15:52:05 -0400

    refactor: :recycle: adds third information block

---
+++ content-protocol-website/src/app/components/information-blocks/block-3.tsx
@@ -0,0 +1,23 @@
+import block2Image from "../../assets/information-blocks/Block_2_Desktop.png";
+
+export default function Block3() {
+  return (
+    <div
+      className="w-full relative"
+      style={{
+        backgroundImage: `url(${block2Image.src})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        aspectRatio: '1441/744',
+        maxHeight: '1000px'
+      }}
+    >
+      <div className="flex justify-between items-center gap-x-16 px-28 h-full">
+        <h3 className="text-white text-center text-7xl">
+          Every piece of content is an ERC-20 token. Every interaction is an opportunity to play, earn, or build.
+        </h3>
+      </div>
+    </div>
+  );
+}
--- content-protocol-website/src/app/page.tsx
+++ content-protocol-website/src/app/page.tsx
@@ -2,6 +2,8 @@ import Hero from "./components/hero/hero";
 import FeaturedImageCarousel from "./components/global/carousel/featured-image-carousel";
 import Block1 from "./components/information-blocks/block-1";
 import Block2 from "./components/information-blocks/block-2";
+import Block3 from "./components/information-blocks/block-3";
+
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
@@ -17,6 +19,10 @@ export default function Home() {
 
       <section className="mt-40">
         <Block2 />
+      </section>
+
+      <section className="mt-40">
+        <Block3 />
       </section>
     </div>
   );
