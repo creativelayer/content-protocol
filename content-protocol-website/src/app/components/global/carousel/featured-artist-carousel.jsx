da111152f2dae844647593285762ab0aba14d1ca
Chris Arsenault <chrisstanarsenault@gmail.com>
2025-04-30 17:26:23 -0400

    refactor: :recycle: updated carousel images

--- content-protocol-website/src/app/components/global/carousel/featured-artist-carousel.jsx
+++ content-protocol-website/src/app/components/global/carousel/featured-artist-carousel.jsx
@@ -1,13 +1,11 @@
-import carouselImage1 from '@/app/assets/carousel/featured-artists/Anna_Dart.png';
-import carouselImage2 from '@/app/assets/carousel/featured-artists/Aoife_O’Dwyer.png';
-import carouselImage3 from '@/app/assets/carousel/featured-artists/Delsol.png';
-import carouselImage4 from '@/app/assets/carousel/featured-artists/ErikX.png';
-import carouselImage5 from '@/app/assets/carousel/featured-artists/Leslie_A_Spurlock.png';
-import carouselImage6 from '@/app/assets/carousel/featured-artists/Mike_Fogg.png';
-import carouselImage7 from '@/app/assets/carousel/featured-artists/Nathan_A_Bauman.png';
-import carouselImage8 from '@/app/assets/carousel/featured-artists/Rich_Armstrong.png';
-import carouselImage9 from '@/app/assets/carousel/featured-artists/Roger_Haus.png';
-import carouselImage10 from '@/app/assets/carousel/featured-artists/uyo66.png';
+import carouselImage1 from '@/app/assets/carousel/featured-artists-v2/Dan_Woo.png';
+import carouselImage2 from '@/app/assets/carousel/featured-artists-v2/Erik_X.png';
+import carouselImage3 from '@/app/assets/carousel/featured-artists-v2/juujuumama.png';
+import carouselImage4 from '@/app/assets/carousel/featured-artists-v2/Leslie_Spurlock.png';
+import carouselImage5 from '@/app/assets/carousel/featured-artists-v2/Nikalaus.png';
+import carouselImage6 from '@/app/assets/carousel/featured-artists-v2/Rich_Armstrong.png';
+import carouselImage7 from '@/app/assets/carousel/featured-artists-v2/Roger_Haus.png';
+import carouselImage8 from '@/app/assets/carousel/featured-artists-v2/Tylersjourney.png';
 
 import styles from './carousel.module.css';
 
@@ -113,30 +111,6 @@ export default function FeaturedImageCarousel() {
           <p className="text-white text-center text-4xl font-medium absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Rich Armstrong</p>
         </div>
 
-        <div className="relative">
-          <div
-            style={{
-              backgroundImage: `url(${carouselImage9.src})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-            }}
-            className="rounded-4xl flex-shrink-0 w-[187px] h-[250px] md:w-[312px] md:h-[416px]"
-          />
-          <p className="text-white text-center text-4xl font-medium absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Roger Haus</p>
-        </div>
-
-        <div className="relative">
-          <div
-            style={{
-              backgroundImage: `url(${carouselImage10.src})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-            }}
-            className="rounded-4xl flex-shrink-0 w-[187px] h-[250px] md:w-[312px] md:h-[416px]"
-          />
-          <p className="text-white text-center text-4xl font-medium absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">uyo66</p>
-        </div>
-
         {/* Duplicate set of images, to keep the carousel infinite */}
         <div className="relative">
           <div
@@ -232,30 +206,6 @@ export default function FeaturedImageCarousel() {
             className="rounded-4xl flex-shrink-0 w-[187px] h-[250px] md:w-[312px] md:h-[416px]"
           />
           <p className="text-white text-center text-4xl font-medium absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Rich Armstrong</p>
-        </div>
-
-        <div className="relative">
-          <div
-            style={{
-              backgroundImage: `url(${carouselImage9.src})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-            }}
-            className="rounded-4xl flex-shrink-0 w-[187px] h-[250px] md:w-[312px] md:h-[416px]"
-          />
-          <p className="text-white text-center text-4xl font-medium absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Roger Haus</p>
-        </div>
-
-        <div className="relative">
-          <div
-            style={{
-              backgroundImage: `url(${carouselImage10.src})`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center'
-            }}
-            className="rounded-4xl flex-shrink-0 w-[187px] h-[250px] md:w-[312px] md:h-[416px]"
-          />
-          <p className="text-white text-center text-4xl font-medium absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">uyo66</p>
         </div>
       </div>
     </div>
